f41231b87848b4bccfb8740e0069d30b6eb70b67
Aryansingh0089 <officialaryansingh6201890044@gmail.com>
2026-06-06 12:56:55 +0530

    Chapter 5 - PS

---
+++ Chapter 5 - PS/08_problem8.py
@@ -0,0 +1,18 @@
+# Q8.> If the language of the two friends are same.
+# What will happen to program in problem 6?
+# sollution.> 
+
+
+d = {"Aryan" : input("enter your fevorite language Aryan :",),
+     "ADKD" : input("enter your fevorite language ADKD :",),
+     "Mulla" : input("enter your fevorite language Mulla :",),
+     "Mishra" : input("enter your fevorite language Mishra :",)
+     }
+# now we have 2 same languages in our dict. 
+print(d.items()) 
+print(d)
+
+# but friends name and there name with there languages
+# will be considered and print(d) will contain both name and there
+# same language
+ 
