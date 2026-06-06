259da775215529c8db5ffbdf0e11938245f9e1a6
Aryansingh0089 <officialaryansingh6201890044@gmail.com>
2026-06-06 13:07:35 +0530

    Chapter 6 - PS

---
+++ Chapter 6 - PS/04_problem4.py
@@ -0,0 +1,9 @@
+# Q4.> Write a program to find wheather a given username contains less than 10
+# charecters or not.
+# Solution.>
+user_name = input("Enter your name :")
+if(len(user_name)<10):
+    print("your user_name contains less then 10 Charcters")
+else:
+    print("you have entered your user_name in correct format.")
+     
