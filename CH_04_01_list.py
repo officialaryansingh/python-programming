57e00e6c1bf43d27fda3a3e7f14f0ba986c26f69
Aryansingh0089 <officialaryansingh6201890044@gmail.com>
2026-06-06 12:25:54 +0530

    Chapter 4
    in this we study about list and it's fundamentals

---
+++ CH_04_01_list.py
@@ -0,0 +1,24 @@
+enemies = ["appel", "orange", 5, 345.06, False, "Aakash", "Rohan"]
+print(enemies)
+print(enemies[2])
+
+print(enemies[0])
+print(enemies[6])
+
+enemies[0] = "monkey" # Unlike strings lists are mutable
+
+
+print(enemies[0])
+print(enemies)
+# a list can be indexed just like the string 
+name = "Aryan"
+print(name[4])
+
+
+# slicing of lists can be done just like we do in case of strings
+
+print(enemies[:5])
+
+
+
+
