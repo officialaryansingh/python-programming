00fc468ed46b2c967b093797e0d69ea744cb452a
Aryansingh0089 <officialaryansingh6201890044@gmail.com>
2026-06-06 12:45:31 +0530

    Chapter 5
    introduction to sets

---
+++ Chapter 5/03_sets.py
@@ -0,0 +1,22 @@
+# "SETS":
+#set is a collection of non repetive elements 
+# as a bignner we can see set as:
+# set in python as data types containing unique values 
+
+
+# method to write an empty set 
+e = set() # don't use s = {} as it will creat an empty dictionary 
+s = {1,2,9,5,8,6,66,6,6,2,6}
+
+print(s) # output: {1,2,9,5,8,6,66,6,6,2,6}
+# order will not be mentained here 
+# every repeted value in a set will be taken only one time
+# means set's output will be elements we non repeated value
+
+# i.e. if you need order we need to use lists
+
+
+# s1 = {1,3,4,34,544,2,1,345,333,3,3,3}
+# print(s1)
+
+
