a7db0365c8a3ec5af7c8fe6fe8ab883824b4eb43
Aryansingh0089 <officialaryansingh6201890044@gmail.com>
2026-06-06 13:02:09 +0530

    Chapter 6
    if_elif_else lader fundamentals

---
+++ Chapter 6/02_if_elif_else_lader.py
@@ -0,0 +1,10 @@
+a = int(input("Enter no. :",))
+
+if(a>= 20):
+    print("you can do it!")
+elif(0<a<20):
+    print("keep it up")
+elif(a<0):
+    print("invalide age")
+else:
+    print("bye bye")
