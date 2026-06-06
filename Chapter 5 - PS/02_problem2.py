89868063178001e830892250b0a3cccdb2ccbf57
Aryansingh0089 <officialaryansingh6201890044@gmail.com>
2026-06-06 12:54:19 +0530

    Chapter 5 - PS

---
+++ Chapter 5 - PS/02_problem2.py
@@ -0,0 +1,20 @@
+# Q2.>Write a program to input eight number from the user and
+# display all the unique numbers (once).
+
+# soln.>
+
+s = set()
+s.add(input("Enter No.1 : ",))
+s.add(input("Enter No.2 : ",))
+s.add(input("Enter No.3: ",))
+s.add(input("Enter No.4 : ",))
+s.add(input("Enter No.5 : ",))
+s.add(input("Enter No.6 : ",))
+s.add(input("Enter No.7 : ",))
+s.add(input("Enter No.8 : ",))
+
+print(s)
+
+# for item in s:
+#     print(item) # doing iteration of the above set formed by the user
+ 
