d8a5976218d1d9ab811a66dd117c5168d4b09e9b
Aryansingh0089 <officialaryansingh6201890044@gmail.com>
2026-06-06 12:55:28 +0530

    Chapter 5 - PS

---
+++ Chapter 5 - PS/06_problem6.py
@@ -0,0 +1,11 @@
+# Q6.> Creat an empty dictionary.Allow 4 friends to enter their favorite language
+# as value and use key as there names.Assumes that the names are unique.
+# solution.>
+
+d = {"Aryan" : input("enter your fevorite language Aryan :",),
+     "ADKD" : input("enter your fevorite language ADKD :",),
+     "Nithin Mullaa": input("enter your fevorite language Mullaa :",),
+     "Mishra" : input("enter your fevorite language Mishra :",)
+     }
+
+print(d.items()) 
